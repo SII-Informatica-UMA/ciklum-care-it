0b971e0b87a2470b37540fe9618e43d2c5fa3c37
antoniotruji <antoniotrujillo@uma.es>
2024-03-31 18:34:03 +0200

    prueba

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -23,7 +23,7 @@ describe('AppComponent', () => {
   it(`should have as title 'practica5'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.sesionElegido).toBeUndefined();
+    expect(app.planElegido).toBeUndefined();
   });
 
   it('should render title', () => {
