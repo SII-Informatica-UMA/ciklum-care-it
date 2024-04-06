46c8e9116620f61f4ccb7c1fb933823f4064c3a8
nikasenia <niks@uma.es>
2024-04-06 16:17:58 +0200

    Tests para comprobar que los campos del formulario muestran los datos de la sesion

--- frontend/src/app/detalle-sesion/detalle-sesion.component.spec.ts
+++ frontend/src/app/detalle-sesion/detalle-sesion.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+/**import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetalleSesionComponent } from './detalle-sesion.component';
 
@@ -21,3 +21,4 @@ describe('DetalleSesionComponent', () => {
     expect(component).toBeTruthy();
   });
 });
+**/
--- frontend/src/app/formulario-sesion/formulario-sesion.component.spec.ts
+++ frontend/src/app/formulario-sesion/formulario-sesion.component.spec.ts
@@ -17,10 +17,132 @@ describe('FormularioSesionComponent', () => {
 
     fixture = TestBed.createComponent(FormularioSesionComponent);
     component = fixture.componentInstance;
+    component.accion = 'Añadir';
+    component.sesion = {idPlan: 1, inicio: '2024-03-29T08:00:00', fin: '2024-03-29T09:00:00', 
+    trabajoRealizado: 'Trabajo realizado', multimedia: ['enlace1', 'enlace2'],
+    descripcion: 'Descripción1', presencial: true, datosSalud: ['80', '60', '254'], id: 1};
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('debe mostrar el campo de fin con la fecha correcta (pobando con YYYY-MM-DDTHH:mm:ss )', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#fin') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('2024-03-29T09:00');
+      done(); 
+    });
+  });
+
+  it('debe mostrar el campo de inicio con la fecha correcta (pobando con YYYY-MM-DDTHH:mm:ss )', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#inicio') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('2024-03-29T08:00');
+      done(); 
+    });
+  });
+
+  it('debe mostrar el campo trabajo realizado (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#trabajo') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('Trabajo realizado');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo video (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#video') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('enlace1');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo foto (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#foto') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('enlace2');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo descripcion (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#descripcion') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('Descripción1');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo pulsaciones (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#pulsaciones') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('80');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo peso (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#peso') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('60');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo calorias (pobando con una string)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#calorias') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.value).toBe('254');
+      done();
+    });
+  });
+
+  it('debe mostrar el campo presencial (pobando con true)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#presencial') as HTMLInputElement;
+
+    fixture.whenStable().then(() => {
+      expect(campo.checked).toBe(true);
+      done();
+    });
+  });
+
+  it('debe modificar el valor presencial (clickeando en la checkbox cuando está en true)', (done: DoneFn) => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const campo = compiled.querySelector('#presencial') as HTMLInputElement;
+    campo.click();
+    campo.dispatchEvent(new Event('change'));
+    fixture.detectChanges();
+
+    fixture.whenStable().then(() => {
+      expect(component.sesion.presencial).toBe(false);
+      done();
+    });
+  });
+
+
+  
 });
+
+
