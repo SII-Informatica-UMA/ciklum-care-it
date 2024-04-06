6e5a28b531639102c92a25f9147f1d64796e6850
Alberto-tp <116553051+Alberto-tp@users.noreply.github.com>
2024-04-06 12:20:27 +0200

    Pruebas
    creacion de pruebas, cambio en los imports y providers de detalle-sesion-component.spec.ts, creacion de la nueva clase sesion.component.spec

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -20,7 +21,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'practica5'`, () => {
+  it(`should have as title 'Sesiones'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.planElegido).toBeUndefined();
--- frontend/src/app/sesion/sesion.component.spec.ts
+++ frontend/src/app/sesion/sesion.component.spec.ts
@@ -1,21 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { DetalleSesionComponent } from './detalle-sesion.component';
+import { SesionComponent } from './sesion.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
 
-describe('DetalleSesionComponent', () => {
-  let component: DetalleSesionComponent;
-  let fixture: ComponentFixture<DetalleSesionComponent>;
+describe('SesionComponent', () => {
+  let component: SesionComponent;
+  let fixture: ComponentFixture<SesionComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetalleSesionComponent ],
+      declarations: [ SesionComponent],
       imports: [FormsModule],
       providers: [NgbActiveModal]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(DetalleSesionComponent);
+    fixture = TestBed.createComponent(SesionComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
