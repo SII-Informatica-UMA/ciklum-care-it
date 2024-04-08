c37043cdcf22a29aef149f8e0a1d94a540012a3d
Alberto-tp <116553051+Alberto-tp@users.noreply.github.com>
2024-04-08 08:52:35 +0200

    Prueba en componente principal

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,9 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -13,6 +17,11 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    compiled = fixture.nativeElement as HTMLElement;
   });
 
   it('should create the app', () => {
@@ -33,4 +42,10 @@ describe('AppComponent', () => {
     const compiled = fixture.nativeElement as HTMLElement;
     expect(compiled.querySelector('.container h1')?.textContent).toContain('Lista de planes');
   });
+  
+  it('debe haber tres botones', () => {
+    expect(compiled.querySelectorAll('button')).toHaveSize(3);
+  });
+
+  
 });
