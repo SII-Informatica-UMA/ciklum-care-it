a79ef43b484995893f9ca042945a0dc307dac5c1
nikasenia <niks@uma.es>
2024-04-23 19:54:00 +0200

    hash and equals for datos salud

--- backend/backend/src/main/java/jpa/backend/entities/DatosSalud.java
+++ backend/backend/src/main/java/jpa/backend/entities/DatosSalud.java
@@ -20,6 +20,7 @@ public class DatosSalud {
         return peso;
     }
 
+
     public void setPeso(Long peso) {
         this.peso = peso;
     }
@@ -35,6 +36,43 @@ public class DatosSalud {
     @Override
     public String toString() {
         return "DatosSalud{" + "pulsaciones=" + pulsaciones + ", peso=" + peso + ", calorias=" + calorias + '}';
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((pulsaciones == null) ? 0 : pulsaciones.hashCode());
+        result = prime * result + ((peso == null) ? 0 : peso.hashCode());
+        result = prime * result + ((calorias == null) ? 0 : calorias.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        DatosSalud other = (DatosSalud) obj;
+        if (pulsaciones == null) {
+            if (other.pulsaciones != null)
+                return false;
+        } else if (!pulsaciones.equals(other.pulsaciones))
+            return false;
+        if (peso == null) {
+            if (other.peso != null)
+                return false;
+        } else if (!peso.equals(other.peso))
+            return false;
+        if (calorias == null) {
+            if (other.calorias != null)
+                return false;
+        } else if (!calorias.equals(other.calorias))
+            return false;
+        return true;
     }
 }
 
