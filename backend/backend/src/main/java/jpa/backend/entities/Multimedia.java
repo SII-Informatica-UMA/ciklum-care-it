7277092a94f6ecc05cd7160f727e00c81010248a
Alberto-tp <116553051+Alberto-tp@users.noreply.github.com>
2024-04-23 19:49:40 +0200

    Cambio en Multimedia.java
    getters, setters, equals y hashcode

--- backend/backend/src/main/java/jpa/backend/entities/Multimedia.java
+++ backend/backend/src/main/java/jpa/backend/entities/Multimedia.java
@@ -5,4 +5,52 @@ import jakarta.persistence.*;
 public class Multimedia {
     private String imagen;
     private String video;
+
+
+    private String getImagen(){
+        return this.imagen;
+    }
+
+    private void setImagen(String img){
+        this.imagen = img;
+    }
+
+    private String getVideo(){
+        return this.video;
+    }
+
+    private void setVideo(String vid){
+        this.video = vid;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((imagen == null) ? 0 : imagen.hashCode());
+        result = prime * result + ((video == null) ? 0 : video.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Multimedia other = (Multimedia) obj;
+        if (imagen == null) {
+            if (other.imagen != null)
+                return false;
+        } else if (!imagen.equals(other.imagen))
+            return false;
+        if (video == null) {
+            if (other.video != null)
+                return false;
+        } else if (!video.equals(other.video))
+            return false;
+        return true;
+    }
 }
