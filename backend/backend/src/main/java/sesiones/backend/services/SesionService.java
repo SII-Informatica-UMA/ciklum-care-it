0f67490c74d9e90d882263c7cd518c5c0d250c7c
Alberto-tp <116553051+Alberto-tp@users.noreply.github.com>
2024-05-23 12:12:07 +0200

    Llamada a microservicio de clientes
    Prueba de llamada al microservicio de clientes ubicado en el puerto 8082

--- backend/backend/src/main/java/sesiones/backend/services/SesionService.java
+++ backend/backend/src/main/java/sesiones/backend/services/SesionService.java
@@ -17,9 +17,11 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import org.springframework.web.util.UriBuilder;
 import org.springframework.web.util.UriBuilderFactory;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import sesiones.backend.entities.Sesion;
 import sesiones.backend.exceptions.SesionInexistenteException;
+import sesiones.backend.exceptions.SesionNoAsociadaException;
 import sesiones.backend.repositories.SesionRepository;
 import sesiones.backend.security.JwtUtil;
 import sesiones.backend.security.SecurityConfguration;
@@ -62,6 +64,25 @@ public class SesionService {
         return peticion;
     }
 
+    private RequestEntity<Void> get(String scheme, String host, int port, String path, String token, String parametro, Long valor_parametro) {
+        URI uri = uri(scheme, host, port, path);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token); // Añadir token de autenticación como Bearer
+
+        String urlTemplate = UriComponentsBuilder.fromHttpUrl(uri.toString())
+                .queryParam(parametro, valor_parametro)
+                .encode()
+                .toUriString();
+
+        var peticion = RequestEntity.get(urlTemplate)
+            .accept(MediaType.APPLICATION_JSON)
+            .headers(headers)
+            .build();
+        return peticion;
+    }
+
+
     public List<Sesion> getSesiones(Long idPlan){
         return repoSesion.findByIdPlan(idPlan);
     }
@@ -79,16 +100,34 @@ public class SesionService {
         String token = jwtUtil.generateToken(user.get());
         user.ifPresent(u -> log.info("token: "+token));
 
-        var peticion = get("http","localhost",8081,"/centro",token);
+        //microservicio centrogerente ejecutado en el puerto 8081 con --server.port=8081
+        var peticionCentros = get("http","localhost",8081,"/centro",token);
 
-        var respuesta = restTemplate.exchange(peticion,
+        var respuestaCentros = restTemplate.exchange(peticionCentros,
         new ParameterizedTypeReference<List<Map<String, Object>>>() {
         });
 
-        log.info("Respuesta: " + respuesta.getStatusCode());
-        if(respuesta.getStatusCode().value()==200){
-            for(Map<String,Object> centro : respuesta.getBody()){
-                log.info("Centro "+centro.get("idCentro").toString());
+        log.info("Respuesta: " + respuestaCentros.getStatusCode());
+        if(respuestaCentros.getStatusCode().value()==200){
+            for(Map<String,Object> centro : respuestaCentros.getBody()){
+                log.info("Centro " + centro.get("idCentro").toString());
+
+                Long idCentro =  Long.parseLong(centro.get("idCentro").toString());
+
+                //ahora vemos los clientes y entrenamientos de cada uno de los centros
+
+                //microservicio clientes en 8082 con --server.port=8082
+                var peticionClientes = get("http","localhost",8082,"/cliente",token, "centro", idCentro);
+
+                 var respuestaClientes = restTemplate.exchange(peticionClientes,
+                new ParameterizedTypeReference<List<Map<String, Object>>>() {
+                });
+
+                log.info("Respuesta: " + respuestaClientes.getStatusCode());
+
+                for(Map<String, Object> cliente: respuestaClientes.getBody()){
+                    log.info("Cliente " + cliente.get("idUsuario").toString());
+                }
             }
         }
         
